b5abd9cfa63d55b7d1a88c67808988b56a8f4326
johnthom11370 <112103129+jcthompson11370@users.noreply.github.com>
2022-10-05 05:00:13 +0000

    created variable "n" and for loop to generate numbers for a selected "n"value

---
+++ HW7_P4/main4.cpp
@@ -0,0 +1,23 @@
+// Programmer: John Thompson
+// Description: HW7 Problem 4
+// Date: 10/ 04/ 2022
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+  int n; // number of days
+
+  cout << "Enter n: ";
+  cin >> n;
+
+  for(int i = 0; i < n; i++)
+    {
+      
+    }
+
+  
+}
