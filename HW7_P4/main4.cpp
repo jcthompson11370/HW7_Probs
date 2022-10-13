bbb94ecb1c33f0c26fbd1acd70f739a216c69de4
johnthom11370 <112103129+jcthompson11370@users.noreply.github.com>
2022-10-13 04:02:31 +0000

    Cleaned up code with outputs

--- HW7_P4/main4.cpp
+++ HW7_P4/main4.cpp
@@ -9,24 +9,26 @@ using namespace std;
 int main(){
   
   int n;
+  cout << "Enter test cases: ";
   cin >> n;    // number of test cases
   
   int count;
+  cout << "Enter " << n << " variables: ";
   cin >> count;    //day numbers mixed
   
   int days = 0; // start from zero
   
   for (int i = 1; i < n; i++) {
     
-    int a;            
+    int a;  // variable for equation in "if" statement    
     cin >> a;
     
-    if (a < count) {    //if a is less than number, then replace it
+    if (a < count) {    //if "a" is less than number, then replace it
       days = i;
       count = a;
         }
     }
-    cout << days; //days to wait
+    cout << "The number of days to wait is: " << days << endl; //days to wait
   
   return 0;
  
