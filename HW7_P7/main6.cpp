17326b737f5007cf73539ea8b0f20eb110c12e5a
johnthom11370 <112103129+jcthompson11370@users.noreply.github.com>
2022-10-13 04:28:00 +0000

    finished user defined functions

--- HW7_P7/main6.cpp
+++ HW7_P7/main6.cpp
@@ -5,10 +5,13 @@
 
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 
 using namespace std;
 
 int totalResistance(double resistance [], int listSize);
+int totalCurrent(double current[], int listSize);
+int totalPower(double power[], int listSize);
 
 
 const int ARRAY_SIZE = 5; // for five inputs
@@ -23,7 +26,31 @@ int totalResistance(double resistance [], int listSize){
   int totalResistance = 0;
 
   for(int index = 0; index < listSize; index++){
+    
     totalResistance += resistance[index];
   }
   return totalResistance;
 }
+
+int totalCurrent(double current[], int listSize){
+
+  int totalCurrent = 0;
+
+  for(int index = 0; index < listSize; index++){
+    
+    totalCurrent += current[index];
+  }
+  return totalCurrent;
+}
+
+int totalPower(double power[], int listSize){
+
+  double totalPower = 0;  // need double to use sci notation
+
+  for(int index = 0; index < listSize; index++){
+    
+    totalPower += power[index];
+  }
+  return totalPower;
+}
+
