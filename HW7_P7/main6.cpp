56322801a7808ad1175784ecec6320059908c672
johnthom11370 <112103129+jcthompson11370@users.noreply.github.com>
2022-10-13 05:10:44 +0000

    could not name functions after the variables.  Changed, and code runs fine now

--- HW7_P7/main6.cpp
+++ HW7_P7/main6.cpp
@@ -9,7 +9,7 @@
 
 using namespace std;
 
-int totalResistance(double resistance [], int listSize);
+int totalResistance(double resistance[], int listSize);
 int totalCurrent(double current[], int listSize);
 int totalPower(double power[], int listSize);
 
@@ -17,40 +17,81 @@ int totalPower(double power[], int listSize);
 const int ARRAY_SIZE = 5; // for five inputs
 
 int main(){
-
   
+  double resistance[ARRAY_SIZE] = {16, 27, 39, 56, 81};
+  double current[ARRAY_SIZE];
+  double power[ARRAY_SIZE];
+
+  for(int i = 0; i < 5; i++){
+    cin >> current[i];
+  }
+
+  for (int i = 0; i < 5; i++){
+    power[i] = resistance[i] * pow(current[i], 2);
+  }
+
+  cout << setprecision(2);
+  cout << setw(10) << "Resistance:"
+       << setw(4) << ' '
+       << setw(10) << "Current:"
+       << setw(4) << ' '
+       << setw(10) << "Power:" 
+       << endl;
+
+  for (int i = 0; i < 5; i++){
+
+    cout << setw(10) << resistance[i]
+       << setw(4) << ' '
+       << setw(10) << current[i]
+       << setw(4) << ' '
+       << setw(10) << power[i] 
+       << endl;  
+  }
+
+  int sumResistance;
+  sumResistance = totalResistance(resistance, ARRAY_SIZE);
+  cout << "Total: " << sumResistance;
+  
+  int sumCurrent;
+  sumCurrent = totalCurrent(current, ARRAY_SIZE);
+  cout << setw(14) << sumCurrent;
+
+  double sumPower;
+  sumPower = totalPower(power, ARRAY_SIZE);
+  cout << setw(14) << sumPower;
+ 
 }
 
 int totalResistance(double resistance [], int listSize){
 
-  int totalResistance = 0;
+  int sumResistance = 0;
 
   for(int index = 0; index < listSize; index++){
     
-    totalResistance += resistance[index];
+    sumResistance += resistance[index];
   }
-  return totalResistance;
+  return sumResistance;
 }
 
 int totalCurrent(double current[], int listSize){
 
-  int totalCurrent = 0;
+  int sumCurrent = 0;
 
   for(int index = 0; index < listSize; index++){
     
-    totalCurrent += current[index];
+    sumCurrent += current[index];
   }
-  return totalCurrent;
+  return sumCurrent;
 }
 
 int totalPower(double power[], int listSize){
 
-  double totalPower = 0;  // need double to use sci notation
+  double sumPower = 0;  // need double to use sci notation
 
   for(int index = 0; index < listSize; index++){
     
-    totalPower += power[index];
+    sumPower += power[index];
   }
-  return totalPower;
+  return sumPower;
 }
 
